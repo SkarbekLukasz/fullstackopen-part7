5b0dccaa87008c80cf6feacab77e72f6e096d2e2
Luke <nycthbre@gmail.com>
2024-08-28 20:48:45 +0200

    Exercise 7.10 completion (extra-bloglist)

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -1,129 +1,156 @@
-import { useState, useEffect, useRef } from 'react'
-import Blogs from './components/Blogs'
-import blogService from './services/blogs'
-import Login from './components/Login'
-import loginService from './services/login'
-import Notification from './components/Notification'
+import { useState, useEffect, useRef } from "react";
+import Blogs from "./components/Blogs";
+import blogService from "./services/blogs";
+import Login from "./components/Login";
+import loginService from "./services/login";
+import Notification from "./components/Notification";
+import { useDispatch } from "react-redux";
+import {
+  setNotification,
+  removeNotification,
+} from "./redux/reducers/notificationReducer";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [user, setUser] = useState(null)
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [message, setMessage] = useState(null)
-  const newBlogFormRef = useRef()
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const newBlogFormRef = useRef();
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    blogService.getAll().then(blogs => setBlogs(sortBlogsByLikes(blogs)))
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(sortBlogsByLikes(blogs)));
+  }, []);
 
   useEffect(() => {
-    const loggedUser = window.localStorage.getItem('localUser')
+    const loggedUser = window.localStorage.getItem("localUser");
     if (loggedUser) {
-      const user = JSON.parse(loggedUser)
-      setUser(user)
+      const user = JSON.parse(loggedUser);
+      setUser(user);
     }
-  }, [])
+  }, []);
 
   const handleUsernameChange = (event) => {
-    setUsername(event.target.value)
-  }
+    setUsername(event.target.value);
+  };
 
   const sortBlogsByLikes = (blogsToSort) => {
     return blogsToSort.toSorted((a, b) => b.likes - a.likes);
   };
 
   const handlePasswordChange = (event) => {
-    setPassword(event.target.value)
-  }
+    setPassword(event.target.value);
+  };
 
   const createBlog = async (newBlog) => {
-    const { title, author, url } = newBlog
+    const { title, author, url } = newBlog;
     try {
-      const blog = { title, author, url }
-      const response = await blogService.saveNewBlog(blog)
-      setBlogs(sortBlogsByLikes(blogs.concat(response)))
-      setMessage(`Successfully added blog ${response.title} by ${response.author}`)
-      newBlogFormRef.current.toggleVisibility()
+      const blog = { title, author, url };
+      const response = await blogService.saveNewBlog(blog);
+      setBlogs(sortBlogsByLikes(blogs.concat(response)));
+      dispatch(
+        setNotification(
+          `Successfully added blog ${response.title} by ${response.author}`
+        )
+      );
+      newBlogFormRef.current.toggleVisibility();
       setTimeout(() => {
-        setMessage(null)
-      }, 5000)
+        dispatch(removeNotification());
+      }, 5000);
     } catch (exception) {
-      setMessage('Failed to create blog')
+      dispatch(setNotification("Failed to create blog"));
       setTimeout(() => {
-        setMessage(null)
-      }, 5000)
+        dispatch(removeNotification());
+      }, 5000);
     }
-  }
+  };
 
   const updateLikesCount = async (updatedBlog) => {
     try {
-      const response = await blogService.updateLikesCount(updatedBlog)
-      const updatedBlogs = blogs.map(blog => blog.id === response.data.id ? { ...blog, likes: response.data.likes } : blog)
-      const sortedBlogs = sortBlogsByLikes(updatedBlogs)
-      setBlogs(sortedBlogs)
-      setMessage(`You liked blog ${updatedBlog.title} by ${updatedBlog.author}`)
+      const response = await blogService.updateLikesCount(updatedBlog);
+      const updatedBlogs = blogs.map((blog) =>
+        blog.id === response.data.id
+          ? { ...blog, likes: response.data.likes }
+          : blog
+      );
+      const sortedBlogs = sortBlogsByLikes(updatedBlogs);
+      setBlogs(sortedBlogs);
+      dispatch(
+        setNotification(
+          `You liked blog ${updatedBlog.title} by ${updatedBlog.author}`
+        )
+      );
       setTimeout(() => {
-        setMessage(null)
-      }, 5000)
+        dispatch(removeNotification());
+      }, 5000);
     } catch (exception) {
-      setMessage('Failed to update like count')
+      dispatch(setNotification("Failed to update like count"));
       setTimeout(() => {
-        setMessage(null)
-      }, 5000)
+        dispatch(removeNotification());
+      }, 5000);
     }
-  }
+  };
 
   const deleteBlog = async (blogToDelete) => {
     try {
-      await blogService.deleteBlog(blogToDelete.id)
-      const updatedBlogs = blogs.filter(blog => blog.id !== blogToDelete.id)
-      setBlogs(sortBlogsByLikes(updatedBlogs))
-      setMessage(`Successfully deleted blog ${blogToDelete.title} by ${blogToDelete.author}`)
+      await blogService.deleteBlog(blogToDelete.id);
+      const updatedBlogs = blogs.filter((blog) => blog.id !== blogToDelete.id);
+      setBlogs(sortBlogsByLikes(updatedBlogs));
+      dispatch(
+        setNotification(
+          `Successfully deleted blog ${blogToDelete.title} by ${blogToDelete.author}`
+        )
+      );
       setTimeout(() => {
-        setMessage(null)
-      }, 5000)
-    } catch(exception) {
-      setMessage('Failed to delete blog')
+        dispatch(removeNotification());
+      }, 5000);
+    } catch (exception) {
+      dispatch(setNotification("Failed to delete blog"));
       setTimeout(() => {
-        setMessage(null)
-      }, 5000)
+        dispatch(removeNotification());
+      }, 5000);
     }
-  }
+  };
 
   const handleLogin = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     try {
-      const user = await loginService.login({ username, password })
-      blogService.setToken(user.token)
-      setUser(user)
-      window.localStorage.setItem('localUser', JSON.stringify(user))
-      setUsername('')
-      setPassword('')
+      const user = await loginService.login({ username, password });
+      blogService.setToken(user.token);
+      setUser(user);
+      window.localStorage.setItem("localUser", JSON.stringify(user));
+      setUsername("");
+      setPassword("");
     } catch (exception) {
-      setMessage('Wrong credentials')
+      dispatch(setNotification("Wrong credentials"));
       setTimeout(() => {
-        setMessage(null)
-      }, 5000)
+        dispatch(removeNotification());
+      }, 5000);
     }
-  }
+  };
 
   const handleLogout = () => {
-    window.localStorage.removeItem('localUser')
-    setUser(null)
-    setMessage('Logged out')
+    window.localStorage.removeItem("localUser");
+    setUser(null);
+    dispatch(setNotification("Logged out"));
     setTimeout(() => {
-      setMessage(null)
-    }, 5000)
-  }
+      dispatch(removeNotification());
+    }, 5000);
+  };
 
   return (
     <div>
       <h1>Blogs list</h1>
-      <Notification message={message} />
-      {user === null ?
-        <Login handleLogin={handleLogin} handlePasswordChange={handlePasswordChange} handleUsernameChange={handleUsernameChange} /> :
+      <Notification />
+      {user === null ? (
+        <Login
+          handleLogin={handleLogin}
+          handlePasswordChange={handlePasswordChange}
+          handleUsernameChange={handleUsernameChange}
+        />
+      ) : (
         <Blogs
           createBlog={createBlog}
           blogs={blogs}
@@ -132,9 +159,10 @@ const App = () => {
           newBlogFormRef={newBlogFormRef}
           updateLikesCount={updateLikesCount}
           deleteBlog={deleteBlog}
-        />}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- bloglist-frontend/src/components/Notification.jsx
+++ bloglist-frontend/src/components/Notification.jsx
@@ -1,18 +1,21 @@
-const Notification = ({ message }) => {
+import { useSelector } from "react-redux";
+
+const Notification = () => {
+  const message = useSelector((state) => state.notification);
 
   const styles = {
-    border: 'solid blue 2px',
-    color: 'black',
-    backgroundColor: 'grey',
-    padding: '4px'
-  }
+    border: "solid blue 2px",
+    color: "black",
+    backgroundColor: "grey",
+    padding: "4px",
+  };
 
-  if(message === null) return null
+  if (message === "") return null;
   return (
     <div>
       <h2 style={styles}>{message}</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Notification
+export default Notification;
